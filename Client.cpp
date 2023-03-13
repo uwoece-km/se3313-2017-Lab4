1ad9936a4330d23ae535852cb0aabdf0934fee7b
jdailly2 <90720926+jdailly2@users.noreply.github.com>
2023-03-13 18:01:28 -0400

    Client and server running
    The client can now send and receive messages from the Server

--- Client.cpp
+++ Client.cpp
@@ -3,9 +3,48 @@
 #include <stdlib.h>
 #include <time.h>
 
+using namespace std;
+
 using namespace Sync;
 
 int main(void)
 {
+	string userInput = " ";
 	std::cout << "I am a client" << std::endl;
+
+	Socket sock("127.0.0.1",3000);
+	std::cout << "Made it here" << std::endl;
+
+
+	//sock.Open();
+	//connects to the server
+	if(sock.Open()){
+
+		std::cout<<"Client has connected"<<endl;
+		while(userInput != "Done"){
+		
+		//asks client to type somthing
+		std::cout << "I am a client please type somthing" << std::endl;
+		std::cin >> userInput;
+
+		//writes to the server
+		sock.Write(ByteArray(userInput));
+		ByteArray alterMessage;
+
+		//reads the return message from the Server
+		sock.Read(alterMessage);
+		std::cout<<"The new message: "<< alterMessage.ToString()<<std::endl;
+
+
+		}
+
+		sock.Close();
+
+
+
+	}
+  
+
+	
+	
 }
--- Server.cpp
+++ Server.cpp
@@ -4,11 +4,50 @@
 #include <time.h>
 #include <list>
 #include <vector>
+#include <iostream>
+#include <cstring>
 
 using namespace Sync;
+using namespace std;
+
+class serverThread: public Thread{
+    public:
+
+    
+
+    virtual long ThreadMain(void) override{
+
+        
+
+
+
+    }
 
 
 int main(void)
 {
-    std::cout << "I am a server." << std::endl;
+   
+
+//Starts the sever  
+SocketServer serSock(3000);
+std::cout << "I am a server running on port 3000." << std::endl;
+
+
+//waits till the client socket connects
+Socket sock(serSock.Accept());
+ByteArray message;
+
+//gets the message from the client
+std::cout << "The bytes of the message: "<< sock.Read(message)<<std::endl;
+std::cout << "The Message: " << message.ToString()<<std::endl;
+
+//alters the message
+string altMessage = message.ToString();
+for (auto & c: altMessage) c = toupper(c);
+std::cout << "The altered Message: " << altMessage<<std::endl;
+
+sock.Write(altMessage);
+
+
+//std::cout << "The message: "<< message.ToString()<<std::endl;
 }
